2a5972c0e8d10e79e9f48f1e68a8bf1bc5734a49
Aeter4nam <aeter4nam@gmail.com>
2017-04-06 13:22:08 +0600

    1.1

--- PrimitiveTypes.java
+++ PrimitiveTypes.java
@@ -1,7 +1,14 @@
 class PrimitiveTypes {
     public static void main(String argc[]) {
         
-        
-        
+        byte byteValue = 127;
+        short shortValue = 32767;
+        int intValue = 2147483647;
+        long longValue = 922338000;
+        float floatValue = 2.7f;
+        double doubleValue = 3.1415926535;
+        char charValue = '#';
+        boolean booleanValue = true;
+            
     }
 }
