d8627157b11652dfa19a39517e2f41cb94c20c8a
Aeter4nam <aeter4nam@gmail.com>
2017-04-07 19:56:16 +0600

    Revert "Revert "Квадрат числа""
    This reverts commit 1972e25b7ed8db95eff59c3ed60c39abaf68cf12.

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 class Main {
     
+    static long square(long longValue) {
+        for(int i = 0; i < 63; i++) {
+            if(i == 61) {
+                longValue -= 1;
+            }
+            longValue *= 2;
+        }
+        return longValue;
+    }
+    
     public static void main(String argc[]) {
         
     }
