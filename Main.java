f0d793126d2c0eeb605f861aa612d07038fb3b90
Yuriy Pavlovich <aeter4nam@gmail.com>
2017-04-13 22:34:23 +0600

    Revert "Изменения"
    This reverts commit 46bd33bdd76eeca154e853f27e3b522587bf3a97.

--- Main.java
+++ Main.java
@@ -1,8 +1,17 @@
 class Main {
     
-    public static void main(String ...argc) {
-        int value = 7 | 6;
-        System.out.println(value);
+    static long square(long longValue) {
+        for(int i = 0; i < 62; i++) {
+            if(i == 61) {
+                longValue -= 1;
+            }
+            longValue *= 2;
+        }
+        return longValue;
+    }
+    
+    public static void main(String argc[]) {
+        System.out.println(square(2));
     }
     
 }
